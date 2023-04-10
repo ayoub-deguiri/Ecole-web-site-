acf273706f0367c3e604ad3b573cdca52f50c86f
Abdelghafouuur <lahnida1abdelghafour@gmail.com>
2023-04-10 09:22:53 +0000

    montag

--- Admin/inscription.php
+++ Admin/inscription.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 // Start the session
 session_start();
@@ -44,8 +43,6 @@ if(!isset($_SESSION['Role']))
                       }
 ?>
 
-=======
->>>>>>> f791e4316a94f78ef30309688a8fe8ebb1e7057d
 <!DOCTYPE html>
 
 <html lang="en" dir="ltr">
@@ -64,7 +61,6 @@ if(!isset($_SESSION['Role']))
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
         crossorigin="anonymous"></script>
-<<<<<<< HEAD
 
         <script src="./scripts/jquery-3.6.3.min.js"></script>
     <script>
@@ -93,8 +89,6 @@ if(!isset($_SESSION['Role']))
           });
             // ajax deleting formation
         </script>
-=======
->>>>>>> f791e4316a94f78ef30309688a8fe8ebb1e7057d
 </head>
 
 <body>
@@ -107,84 +101,55 @@ if(!isset($_SESSION['Role']))
     </div>
     <ul class="nav-list">
       <li >
-<<<<<<< HEAD
         <a href="acceuil.php">
-=======
-        <a href="acceuil.html">
->>>>>>> f791e4316a94f78ef30309688a8fe8ebb1e7057d
           <i class='bx bx-home'></i>
           <span class="links_name">Acceuil</span>
         </a>
         <span class="tooltip">Acceuil</span>
       </li>
       <li class="NavSelect">
-<<<<<<< HEAD
         <a href="inscription.php">
-=======
-        <a href="inscription.html">
->>>>>>> f791e4316a94f78ef30309688a8fe8ebb1e7057d
           <i class='bx bx-book-bookmark'></i>
           <span class="links_name">Inscriptions</span>
         </a>
         <span class="tooltip">Inscriptions</span>
       </li>
       <li>
-<<<<<<< HEAD
         <a href="formation.php">
-=======
-        <a href="formation.html">
->>>>>>> f791e4316a94f78ef30309688a8fe8ebb1e7057d
           <i class='bx bx-chat'></i>
           <span class="links_name">Formations</span>
         </a>
         <span class="tooltip">Formations</span>
       </li>
       <li>
-<<<<<<< HEAD
         <a href="modifierNombres.php">
-=======
-        <a href="modifierNombres.html">
->>>>>>> f791e4316a94f78ef30309688a8fe8ebb1e7057d
           <i class='bx bx-pie-chart-alt-2'></i>
           <span class="links_name">Modifier Nombres</span>
         </a>
         <span class="tooltip">Modifier Nombres</span>
       </li>
       <li>
-<<<<<<< HEAD
         <a href="gererimages.php">
-=======
-        <a href="gererimages.html">
->>>>>>> f791e4316a94f78ef30309688a8fe8ebb1e7057d
           <i class='bx bx-images'></i>
           <span class="links_name">Gestion Des Images</span>
         </a>
         <span class="tooltip">Gestion Des Images</span>
       </li>
       <li>
-<<<<<<< HEAD
         <a href="gererComptes.php">
-=======
-        <a href="gererComptes.html">
->>>>>>> f791e4316a94f78ef30309688a8fe8ebb1e7057d
           <i class='bx bx-user'></i>
           <span class="links_name">Gestion Des Comptes</span>
         </a>
         <span class="tooltip">Gestion Des Comptes</span>
       </li>
       <li>
-<<<<<<< HEAD
         <a href="modifierProfile.php">
-=======
-        <a href="modifierProfile.html">
->>>>>>> f791e4316a94f78ef30309688a8fe8ebb1e7057d
           <i class='bx bx-cog'></i>
           <span class="links_name">Modifier Profile</span>
         </a>
         <span class="tooltip">Modifier Profile</span>
       </li>
       <li class="profile">
-<<<<<<< HEAD
                 <div class="profile-details">
                     <img src="../images/homme-daffaire.png" alt="profileImg">
                     <div class="name_job">
@@ -196,20 +161,6 @@ if(!isset($_SESSION['Role']))
                 <i class='bx bx-log-out' id="log_out"></i>
                 </a>
             </li>
-=======
-        
-        <div class="profile-details">
-          <img src="../images/homme-daffaire.png" alt="profileImg">
-          <div class="name_job">
-            <div class="name">Prem Shahi</div>
-            <div class="job">Web designer</div>
-          </div>
-        </div>
-       
-        <i class='bx bx-log-out' id="log_out"></i>
-        
-      </li>
->>>>>>> f791e4316a94f78ef30309688a8fe8ebb1e7057d
     </ul>
   </div>
 
@@ -217,11 +168,7 @@ if(!isset($_SESSION['Role']))
 
     <!-- start home section-->
     <section class="home-section">
-<<<<<<< HEAD
     <div class="text">Espace <?php echo $_SESSION["Role"]." : Bonjour ". $_SESSION['Nom'].' '.$_SESSION['Prenom']; ?> </div>
-=======
-        <div class="text">Bonjour Mr Jamal Alfaa</div>
->>>>>>> f791e4316a94f78ef30309688a8fe8ebb1e7057d
         <div class="row">
             <div class="col-md-2"></div>
             <div class="col-md-8 chit-chat-layer1-left">
@@ -229,7 +176,6 @@ if(!isset($_SESSION['Role']))
                     <div class="chit-chat-heading">Manage Inscription</div>
                     <div class="filtrage">
                         <div class="select">
-<<<<<<< HEAD
                         <label for="select"> Type Formation</label>
                         <form method="POST" action="">
                       <select name="FilterType" onchange="this.form.submit()" class="form-select form-select-sm" aria-label=".form-select-lg example">
@@ -249,21 +195,6 @@ if(!isset($_SESSION['Role']))
                         </div>
                     </div>
                     <form method="POST" action="" onclick="this.form.submit()">
-=======
-                            <label for="select"> Type Formation</label>
-                            <select class="form-select form-select-sm" aria-label=".form-select-lg example">
-                                <option selected>Open this select menu</option>
-                                <option value="1">One</option>
-                                <option value="2">Two</option>
-                                <option value="3">Three</option>
-                            </select>
-                        </div>
-                        <div class="date">
-                            <label for="date">Date</label>
-                            <input type="date" name="date" id="date" />
-                        </div>
-                    </div>
->>>>>>> f791e4316a94f78ef30309688a8fe8ebb1e7057d
                     <div class="table-responsive">
                         <table class="table table-hover">
                             <thead>
@@ -278,7 +209,6 @@ if(!isset($_SESSION['Role']))
                                 </tr>
                             </thead>
                             <tbody>
-<<<<<<< HEAD
                             <?php
                 if(!empty($result))
                 {
@@ -308,65 +238,6 @@ if(!isset($_SESSION['Role']))
                 }
         ?>
                         </form>
-=======
-                                <tr>
-                                    <td>1</td>
-                                    <td>Face book</td>
-                                    <td>Malorum</td>
-                                    <td>1</td>
-                                    <td>Face book</td>
-                                    <td>Malorum</td>
-                                    <td>
-                                        <button type="button" class="btn btn-info" data-bs-toggle="modal"
-                                            data-bs-target="#exampleModal">
-                                            details
-                                        </button>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>1</td>
-                                    <td>Face book</td>
-                                    <td>Malorum</td>
-                                    <td>1</td>
-                                    <td>Face book</td>
-                                    <td>Malorum</td>
-                                    <td>
-                                        <button type="button" class="btn btn-info" data-bs-toggle="modal"
-                                            data-bs-target="#exampleModal">
-                                            details
-                                        </button>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>1</td>
-                                    <td>Face book</td>
-                                    <td>Malorum</td>
-                                    <td>1</td>
-                                    <td>Face book</td>
-                                    <td>Malorum</td>
-                                    <td>
-                                        <button type="button" class="btn btn-info" data-bs-toggle="modal"
-                                            data-bs-target="#exampleModal">
-                                            details
-                                        </button>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>1</td>
-                                    <td>Face book</td>
-                                    <td>Malorum</td>
-                                    <td>1</td>
-                                    <td>Face book</td>
-                                    <td>Malorum</td>
-                                    <td>
-                                        <button type="button" class="btn btn-info" data-bs-toggle="modal"
-                                            data-bs-target="#exampleModal">
-                                            details
-                                        </button>
-                                    </td>
-                                </tr>
-                            </tbody>
->>>>>>> f791e4316a94f78ef30309688a8fe8ebb1e7057d
                         </table>
                     </div>
                 </div>
@@ -388,56 +259,6 @@ if(!isset($_SESSION['Role']))
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
-<<<<<<< HEAD
-=======
-                    <table class="table table-hover">
-                        <tbody>
-                            <tr>
-                                <th>Nom Complet</th>
-                                <td>test test</td>
-                            </tr>
-                            <tr>
-                                <th>CIN</th>
-                                <td>ee670894</td>
-                            </tr>
-                            <tr>
-                                <th>Adresse</th>
-                                <td>loudaya city</td>
-                            </tr>
-                            <tr>
-                                <th>Telephone</th>
-                                <td>066578945</td>
-                            </tr>
-                            <tr>
-                                <th>Email</th>
-                                <td>vodka@gmail.com</td>
-                            </tr>
-                            <tr>
-                                <th>Niveau Scolaire</th>
-                                <td>bdoun</td>
-                            </tr>
-                            <tr>
-                                <th>Type</th>
-                                <td>formation</td>
-                            </tr>
-                            <tr>
-                                <th>Le Choix</th>
-                                <td>choix choix xhoix 1</td>
-                            </tr>
-                            <tr>
-                                <th>Date</th>
-                                <td>22/11/2002</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                    <div class="photos">
-                        <img src="../images/diplome/LICENCE.jpg" alt="" id="img" />
-                        <img src="../images/diplome/LICENCE.jpg" alt="" id="img" />
-                        <img src="../images/diplome/LICENCE.jpg" alt="" id="img" />
-                        <img src="../images/diplome/LICENCE.jpg" alt="" id="img" />
-                        <img src="../images/diplome/LICENCE.jpg" alt="" id="img" />
-                    </div>
->>>>>>> f791e4316a94f78ef30309688a8fe8ebb1e7057d
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-info" data-bs-dismiss="modal">
